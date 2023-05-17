000eb8f50971f9eaa0fb546465fe332d6cb69f8f
djafarxyxy <farhanch12344@gmail.com>
2023-05-17 20:46:16 +0700

    konstructor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -12,27 +12,33 @@ public:
 	void isiData();
 };
 //definisi member function
-angka::angka(int i) { //constructor
+angka::angka(int i){ //constructor
 	panjang = i;
 	arr = new int[i];
 	isiData();
 }
 
-angka::~angka() { //destructor
+angka::~angka(){ //destructor
 	cout << endl;
 	cetakData();
 	delete[]arr;
 	cout << "Alamat array sudah Dilepaskan" << endl;
 }
 void angka::cetakData() {
-	for (int i = 1;i <= panjang;i++) {
-		cout << i << " = " << arr[i] << endl;
+	for (int i=1;i <= panjang;i++) {
+		cout<<i<<" = "<<arr[i]<<endl;
 	}
 }
 
+void angka::isiData(){
+	for (int i = 1;i <= panjang;i++){
+		cout<<i<<" = ";cin>>arr[i];
+	}
+	cout<<endl;
+}
 int main() {
 	angka belajarcpp(3); //constructor dipanggil
-	angka* ptrBelajarcpp = new angka(5); //constructor
+	angka *ptrBelajarcpp = new angka(5); //constructor
 	delete ptrBelajarcpp; //Destructor di panggil
 
 	return 0;
